f1fbb73f515646e501d64126b538e00d6f993b8e
Brandt Daniels <brandtdaniels@gmail.com>
2023-09-06 21:49:02 -0700

    Update dependencies and fix deprecations

--- string-calculator/swift/StringCalculator/Package.swift
+++ string-calculator/swift/StringCalculator/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.1")
+        .package(url: "https://github.com/Quick/Quick.git", from: "6.1.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "12.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
