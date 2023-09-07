f5a7594abc789a005c80ab9cbe4cffb9b8e9805b
Brandt Daniels <brandtdaniels@gmail.com>
2023-09-06 21:59:55 -0700

    Update Package.swift and move BowlingGame.swift into BowlingGame folder

--- bowling-game/swift/bowling-game/Package.swift
+++ bowling-game/swift/bowling-game/Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 5.8
+
 import PackageDescription
 
 let package = Package(
-    name: "BowlingGame"
+    name: "BowlingGame",
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BowlingGame",
+            dependencies: []),
+        .testTarget(
+            name: "BowlingGameTests",
+            dependencies: ["BowlingGame"])
+    ]
 )
